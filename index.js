f5c8cadfb70e7c87fb7a437a02c381ce3ffaae3d
Leandro Tami <leandro.tami@gmail.com>
2024-07-26 13:13:02 -0300

    added incoming folder where new uploads are saved

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const { data } = require('./data.js');
-const port = process.env.PORT || 3000; // Use the port provided by the host or default to 3000
+const multer = require('multer');
+const path = require('path');
 
+const port = process.env.PORT || 3000; // Use the port provided by the host or default to 3000
 const app = express();
-  
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'incoming/');
+    },
+    limits: {
+        fileSize: 5000000 // 5mb
+    },
+    fileFilter: (req, file, callback) => {
+        if (!file.originalname.match(/\.(png|jpeg|jpg)$/i)) {
+            return callback(new Error('Please upload a png or jpeg photo'))
+        }
+        callback(undefined, true);
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+const upload = multer({ storage });
+
 // Middleware to parse JSON requests
 app.use(express.json());
 
@@ -18,6 +38,10 @@ app.post('/items', (req, res) => {
 app.get('/items', (req, res) => {
   res.json(data);
 });
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+    res.status(201).json({message: "successfully uploaded"})
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
