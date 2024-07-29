b6e33316b7ee31fa539ee47d178ed29262ac705c
Leandro Tami <leandro.tami@gmail.com>
2024-07-29 19:09:02 -0300

    reorganized code in index file into server and services

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const multer = require('multer')
+const fs = require('fs')
+const { services } = require('./services.js')
+
+const C_TEMP_DIRECTORY = 'temp/'
+
+class Server {
+
+    #app = express()
+    #upload    
+    
+    constructor() {
+        const storage = multer.diskStorage({
+            destination: (req, file, cb) => {
+                this.#getDestinationDirectory(req, file, cb)
+            },
+            filename: (req, file, cb) => {
+                this.#getFilename(req, file, cb)
+            }
+        })
+        this.#upload = multer({ storage });
+        this.#setRoutes()
+    }
+
+    start(port) {
+        this.#app.listen(port, async () => {
+            console.log(`Server listening on port ${port}`);
+            // await tasks.initDatabase()
+        });
+    }
+
+    #getDestinationDirectory(req, file, cb) {
+        if (!fs.existsSync(C_TEMP_DIRECTORY)) fs.mkdirSync(C_TEMP_DIRECTORY)
+        cb(null, C_TEMP_DIRECTORY);
+    }
+
+    #getFilename(req, file, cb) {
+        cb(null, file.originalname)
+    }
+
+    #setRoutes() {
+
+        this.#app.get('/media', async (req, res) => {
+            services.getMedia(req, res)
+        })
+
+        this.#app.get('/file', async (req, res) => {
+            services.getMediaFile(req, res)
+        })
+
+        this.#app.get('/thumb', async (req, res) => {
+            services.getMediaThumbnail(req, res)
+        })
+
+        this.#app.post('/media', this.#upload.single('media'), async (req, res) => {  
+            services.postMedia(req, res)
+        })
+
+    }
+
+}
+
+exports.server = new Server()
---
+++ services.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const fs = require('fs')
+const { data } = require('./data.js')
+const { tasks } = require('./tasks.js')
+
+exports.services = {
+
+    getMedia: async (req, res) => {
+        const photos = await data.getAll()
+        res.json(photos);
+    },
+
+    getMediaFile: async (req, res) => {
+        const fullPath = await data.getFileFullPath(req.query.id)
+        if (!fullPath) res.status(404).json({message: "not found"})
+        else res.download(fullPath);
+    },
+
+    getMediaThumbnail: async (req, res) => {
+        const fullPath = await data.getFileFullPath(req.query.id)
+        if (!fullPath) res.status(404).json({message: "not found"})
+        else {
+            const file = path.parse(fullPath)
+            const thumb = `${file.dir}/thumbs/${file.name}.jpg`
+            res.download(thumb);
+        }   
+    },
+
+    postMedia: async (req, res) => {
+        const photo = await tasks.addMedia(req.file.filename, req.file.originalname)
+        await tasks.saveExif(photo.media_id, req.file.path)
+        const filePath = await tasks.relocateMedia(photo.media_id, req.file.path)
+        tasks.makeThumbnail(photo.media_id, filePath)
+        tasks.runOCR(photo.media_id, filePath)
+        res.status(201).json({
+            media_id: photo.media_id,
+            message: "success"
+        })    
+    }
+
+}
