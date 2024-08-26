3423810b208140dbbf2a195a165051d9ef9e3f36
Leandro Tami <leandro.tami@gmail.com>
2024-08-26 00:03:57 -0300

    added completely unnecessary but fun ASCII art

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import { initAI } from './tasks/AI.js'
 import { initData } from './data/index.js'
 import msg from './log.js'
 
+
+console.log('\n\
+  __  __ ___ ___ ___   _   ___   _   ___ ___ \n\
+ |  \\/  | __|   \\_ _| /_\\ | _ ) /_\\ / __| __|\n\
+ | |\\/| | _|| |) | | / _ \\| _ \\/ _ \\\\__ \\ _|\n\
+ |_|  |_|___|___/___/_/ \\_\\___/_/ \\_\\___/___|\n\
+\n\
+')
 msg.log('Preparing components before starting server')
 await initData()
 await initAI()
